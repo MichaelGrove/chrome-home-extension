82805dc126be043c28de1c317463f7d059168b6c
Mikael Lehto <mikael.leh@hotmail.com>
2020-07-19 19:12:04 +0300

    Quotes, font change

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,92 @@
+var quotes = [
+	"“Success is not final, failure is not fatal: it is the courage to continue that counts.”\n\n― Winston S. Churchill",
+	"“You have enemies? Good. That means you've stood up for something, sometime in your life.”\n\n― Winston S. Churchill",
+	"“If you are going through hell, keep going.”\n\n― Winston S. Churchill",
+	"“My tastes are simple: I am easily satisfied with the best.”\n\n― Winston S. Churchill",
+	"“Success is stumbling from failure to failure with no loss of enthusiasm.”\n\n― Winston S. Churchill",
+	"“Tact is the ability to tell someone to go to hell in such a way that they look forward to the trip.”\n\n― Winston S. Churchill",
+	"“It is not enough that we do our best; sometimes we must do what is required.”\n\n― Winston S. Churchill",
+	"“The best argument against democracy is a five-minute conversation with the average voter.”\n\n― Winston S. Churchill",
+	"“A lady came up to me one day and said 'Sir! You are drunk', to which I replied 'I am drunk today madam, and tomorrow I shall be sober but you will still be ugly.”\n\n― Winston S. Churchill",
+	"“Nothing in life is so exhilarating as to be shot at without result.”\n\n― Winston S. Churchill",
+	"“Kites rise highest against the wind, not with it.”\n\n― Winston S. Churchill",
+	"“Never give in. Never give in. Never, never, never, never—in nothing, great or small, large or petty—never give in, except to convictions of honour and good sense. Never yield to force. Never yield to the apparently overwhelming might of the enemy.”\n\n― Winston S. Churchill, Never Give In!: The Best of Winston S. Churchill's Speeches",
+	"“Personally, I'm always ready to learn, although I do not always like being taught.”\n\n― Winston S. Churchill",
+	"“For myself I am an optimist - it does not seem to be much use to be anything else.”\n\n― Winston S. Churchill",
+	"“A fanatic is one who can't change his mind and won't change the subject.”\n\n― Winston S. Churchill",
+	"“The greatest lesson in life is to know that even fools are right sometimes.”\n\n― Winston S. Churchill",
+	"“We make a living by what we get. We make a life by what we give.”\n\n― Winston S. Churchill",
+	"“The price of greatness is responsibility.”\n\n― Winston S. Churchill",
+	"“Continuous effort - not strength or intelligence - is the key to unlocking our potential.”\n\n― Winston S. Churchill",
+	"“Attitude is a little thing that makes a big difference.”\n\n― Winston S. Churchill",
+	"“If you cannot read all your books...fondle them---peer into them, let them fall open where they will, read from the first sentence that arrests the eye, set them back on the shelves with your own hands, arrange them on your own plan so that you at least know where they are. Let them be your friends; let them, at any rate, be your acquaintances.”\n\n― Winston S. Churchill",
+	"“An appeaser is one who feeds a crocodile, hoping it will eat him last.”\n\n― Winston S. Churchill",
+	"“To improve is to change; to be perfect is to change often.”\n\n― Winston S. Churchill",
+	"“To each there comes in their lifetime a special moment when they are figuratively tapped on the shoulder and offered the chance to do a very special thing, unique to them and fitted to their talents. What a tragedy if that moment finds them unprepared or unqualified for that which could have been their finest hour.”\n\n― Sir Winston S. Churchill",
+	"“Everyone is in favor of free speech. Hardly a day passes without its being extolled, but some people's idea of it is that they are free to say what they like, but if anyone else says anything back, that is an outrage.”\n\n― Winston S. Churchill",
+	"“We are masters of the unsaid words, but slaves of those we let slip out.”\n\n― Winston S. Churchill",
+	"“The farther backward you can look, the farther forward you are likely to see.”\n\n― Winston S. Churchill",
+	"“You will never reach your destination if you stop and throw stones at every dog that barks.”\n\n― Winston S. Churchill",
+	"“When the eagles are silent, the parrots begin to jabber.”\n\n― Winston S. Churchill",
+	"“Criticism may not be agreeable, but it is necessary. It fulfils the same function as pain in the human body; it calls attention to the development of an unhealthy state of things. If it is heeded in time, danger may be averted; if it is suppressed, a fatal distemper may develop.\"”\n\n”\n\n[New Statesman interview, 7 January 1939]”\n\n― Winston S. Churchill",
+	"“All the great things are simple, and many can be expressed in a single word: freedom, justice, honor, duty, mercy, hope”\n\n― Winston S. Churchill",
+	"“If you have an important point to make, don't try to be subtle or clever. Use a pile driver. Hit the point once. Then come back and hit it again. Then hit it a third time - a tremendous whack.”\n\n― Winston S. Churchill",
+	"“In the course of my life, I have often had to eat my words, and I must confess that I have always found it a wholesome diet.”\n\n― Winston S. Churchill",
+	"“You ask, what is our aim? I can answer in one word. It is victory, victory at all costs, victory in spite of all terror, victory, however long and hard the road may be; for without victory, there is no survival.”\n\n― Winston S. Churchill",
+	"“Now this is not the end. It is not even the beginning of the end.\nBut it is, perhaps, the end of the beginning.”\n\n― Winston S. Churchill, Their Finest Hour",
+	"“When there is no enemy within, the enemies outside cannot hurt you.”\n\n― Winston S. Churchill",
+	"“Sometimes doing your best is not good enough. Sometimes you must do what is required.”\n\n― Winston S. Churchill",
+	"“We have two ears and one mouth, so we should listen more than we say.”\n\n― Zeno of Citium",
+	"“Man conquers the world by conquering himself.”\n\n― Zeno of Citium",
+	"“Well-being is attained little by little, and nevertheless is no little thing itself.”\n\n― Zeno of Citium",
+	"“The only true wisdom is in knowing you know nothing.”\n\n― Socrates",
+	"“I cannot teach anybody anything. I can only make them think”\n\n― Socrates",
+	"“There is only one good, knowledge, and one evil, ignorance.”\n\n― Socrates",
+	"“Be kind, for everyone you meet is fighting a hard battle.”\n\n― Socrates",
+	"“Wonder is the beginning of wisdom.”\n\n― Socrates",
+	"“Strong minds discuss ideas, average minds discuss events, weak minds discuss people.”\n\n― Socrates",
+	"“To find yourself, think for yourself.”\n\n― Socrates",
+	"“Education is the kindling of a flame, not the filling of a vessel.”\n\n― Socrates",
+	"“By all means marry; if you get a good wife, you’ll become happy; if you get a bad one, you’ll become a philosopher.”\n\n― Socrates",
+	"“He who is not contented with what he has, would not be contented with what he would like to have.”\n\n― Socrates",
+	"“Be slow to fall into friendship, but when you are in, continue firm and constant.”\n\n― Socrates",
+	"“If you don't get what you want, you suffer; if you get what you don't want, you suffer; even when you get exactly what you want, you still suffer because you can't hold on to it forever. Your mind is your predicament. It wants to be free of change. Free of pain, free of the obligations of life and death. But change is law and no amount of pretending will alter that reality.”\n\n― Socrates",
+	"“No man has the right to be an amateur in the matter of physical training. It is a shame for a man to grow old without seeing the beauty and strength of which his body is capable.”\n\n― Socrates",
+	"“When the debate is lost, slander becomes the tool of the loser.”\n\n― Socrates",
+	"“The secret of happiness, you see, is not found in seeking more, but in developing the capacity to enjoy less.”\n\n― Socrates",
+	"“Let him who would move the world first move himself.”\n\n― Socrates",
+	"“Employ your time in improving yourself by other men's writings so that you shall come easily by what others have labored hard for.”\n\n― Socrates",
+	"“Do not do to others what angers you if done to you by others.”\n\n― Socrates",
+	"“You have power over your mind - not outside events. Realize this, and you will find strength.”\n\n― Marcus Aurelius, Meditations",
+	"“Dwell on the beauty of life. Watch the stars, and see yourself running with them.”\n\n― Marcus Aurelius, Meditations",
+	"“The happiness of your life depends upon the quality of your thoughts.”\n\n― Marcus Aurelius, Meditations",
+	"“Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth.”\n\n― Marcus Aurelius, Meditations",	
+	"“If you are distressed by anything external, the pain is not due to the thing itself, but to your estimate of it; and this you have the power to revoke at any moment.”\n\n― Marcus Aurelius, Meditations",
+	"“The soul becomes dyed with the colour of its thoughts.”\n\n― Marcus Aurelius, Meditations",
+	"“Our life is what our thoughts make it.”\n\n― Marcus Aurelius, Meditations",
+	"“Never let the future disturb you. You will meet it, if you have to, with the same weapons of reason which today arm you against the present.”\n\n― Marcus Aurelius, Meditations",
+	"“If someone is able to show me that what I think or do is not right, I will happily change, for I seek the truth, by which no one was ever truly harmed. It is the person who continues in his self-deception and ignorance who is harmed.”\n\n― Marcus Aurelius, Meditations",
+	"“If it is not right do not do it; if it is not true do not say it.”\n\n― Marcus Aurelius, Meditations",
+	"“Whenever you are about to find fault with someone, ask yourself the following question: What fault of mine most nearly resembles the one I am about to criticize?”\n\n― Marcus Aurelius, Meditations",
+	"“When another blames you or hates you, or people voice similar criticisms, go to their souls, penetrate inside and see what sort of people they are. You will realize that there is no need to be racked with anxiety that they should hold any particular opinion about you.”\n\n― Marcus Aurelius, Meditations",
+	"“How much more grievous are the consequences of anger than the causes of it.”\n\n― Marcus Aurelius, Meditations",
+	"“The first rule is to keep an untroubled spirit. The second is to look things in the face and know them for what they are.”\n\n― Marcus Aurelius, Meditations",
+	"“Here is a rule to remember in future, when anything tempts you to feel bitter: not \"This is misfortune,\" but \"To bear this worthily is good fortune\".”\n\n― Marcus Aurelius, Meditations",
+	"“Begin each day by telling yourself: Today I shall be meeting with interference, ingratitude, insolence, disloyalty, ill-will, and selfishness – all of them due to the offenders’ ignorance of what is good or evil.”\n\n― Marcus Aurelius, Meditations",
+	"“Look well into thyself; there is a source of strength which will always spring up if thou wilt always look.”\n\n― Marcus Aurelius, Meditations",
+	"“How much time he gains who does not look to see what his neighbour says or does or thinks, but only at what he does himself, to make it just and holy.”\n\n― Marcus Aurelius, Meditations",
+];
+
 window.addEventListener('DOMContentLoaded', function() {
 	function HomePage() {
 		this.init = function() {
 			this.GreetingEl = document.getElementById('Greeting');
 			this.CurrentTimeEl = document.getElementById('CurrentTime');
+			this.QuoteEl = document.getElementById('Quote');
 
 			this.setTime();
 			this.setGreeting();
+			this.setRandomQuote();
 			return this;
 		}
 
@@ -24,6 +105,10 @@ window.addEventListener('DOMContentLoaded', function() {
 				text = "Good evening";
 			}
 			this.GreetingEl.innerText = text;
+		}
+
+		this.setRandomQuote = function () {
+			this.QuoteEl.innerText = quotes[Math.floor(Math.random() * quotes.length)];
 		}
 
 		return this.init();
